f4bf01cc2eac8cff6dc53f799cadbdbef961c7fb
aman-senpai <jemscollin1@gmail.com>
2026-07-04 13:21:00 +0530

    Initial Commit

---
+++ Zenshelf/ZenshelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenshelfApp.swift
+//  Zenshelf
+//
+//  Created by Aman Raj on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ZenshelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
